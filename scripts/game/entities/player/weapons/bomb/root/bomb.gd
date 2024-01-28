62755ae38857844e9e3eb5741bf3b7b4b1a11246
Arthur Mazzardo Naue <arthur_naue@Hotmail.com>
2024-01-28 20:26:02 -0300

    nova animacao pra bomba

--- scripts/game/entities/player/weapons/bomb/root/bomb.gd
+++ scripts/game/entities/player/weapons/bomb/root/bomb.gd
@@ -2,46 +2,28 @@ extends Node2D
 class_name Bomb
 
 @export var weaponCooldown: float
-@export var explosionScene: PackedScene
-@onready var weaponsNode = get_parent()
+@export var projectileScene: PackedScene
 @onready var player = get_parent().get_parent()
 @onready var anim = $anim
 
 func _ready():
 	#reseta o cooldown
 	player.weaponCooldown = 2
-	#reseta a animacao
+	#toca a animacao de ficar parado
 	anim.play("idle")
 
 func _process(delta:float):
 	#verifica se o botao esquerdo do mouse foi clicado
 	if Input.is_action_pressed("mouse_left"):
 		#verifica se o cooldown acabou
-			#atira a bomba
 		if player.weaponCooldown <= 0:
-			anim.play("attack")
-			weaponsNode.attacking = true
+			#atira a bomba
+			Throw()
 
-#funcao realizada ao fim da animacao de ataque
-func _on_anim_animation_finished(attack):
-	#explode
-	explode()
-
-#funcao de explodir
-func explode():
-	#cria o objeto de explosao
-	var explosion = explosionScene.instantiate() as StaticBody2D
-	explosion.global_position = global_position
-	player.get_parent().add_child(explosion)
-	#reseta a animacao
-	anim.play("idle")
+func Throw():
+	#cria o projetil
+	var projectile = projectileScene.instantiate() as CharacterBody2D
+	projectile.global_position = global_position
+	player.get_parent().add_child(projectile)
 	#reseta o cooldown
 	player.weaponCooldown = weaponCooldown
-	weaponsNode.attacking = false
-	$areaCol/col.disabled = true
-
-func _on_area_col_area_entered(area):
-	#verifica se a colisao ocorreu com um inimigo
-	if area.is_in_group("enemy"):
-		#explode
-		explode()
