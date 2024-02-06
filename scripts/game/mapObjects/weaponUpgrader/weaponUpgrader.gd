313253f9464d9541aeb5c26c800ae052af208f8e
Arthur Mazzardo Naue <arthur_naue@Hotmail.com>
2024-02-06 09:59:29 -0300

    weaponUpgrader

---
+++ scripts/game/mapObjects/weaponUpgrader/weaponUpgrader.gd
@@ -0,0 +1,21 @@
+extends StaticBody2D
+class_name WeaponUpgrader
+
+@export var game: Node2D
+@export var player: CharacterBody2D
+@onready var interactText = $interactText
+@onready var priceText = $priceText
+
+func _process(_delta):
+	if interactText.visible == true:
+		if Input.is_action_just_pressed("E"):
+			pass
+
+func _on_hitbox_area_entered(area):
+	if area.is_in_group("player"):
+		interactText.visible = true
+		priceText.visible = true
+
+func _on_hitbox_area_exited(_area):
+	interactText.visible = false
+	priceText.visible = false
