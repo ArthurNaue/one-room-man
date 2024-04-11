4e7ef97a66a76e5df5fbe309ef2601e9e36a025f
Arthur Mazzardo Naue <arthur_naue@Hotmail.com>
2024-04-11 18:29:43 -0300

    melhorei o mago

--- scripts/game/entities/enemies/normal/mage/root/mage.gd
+++ scripts/game/entities/enemies/normal/mage/root/mage.gd
@@ -1,22 +1,22 @@
 extends CharacterBody2D
 class_name Mage
 
+#variaveis
 @export var speed: int
 @onready var player = get_tree().get_first_node_in_group("player")
-var direction: Vector2
 
 func _physics_process(_delta):
+	#cria a variavel de distancia
 	var distance = player.global_position - global_position
+	#verifica a distancia entre o inimigo e o destino
 	if distance.length() < 100:
-		velocity = direction * speed
+		#inverte a velocidade aplicada no inimigo
+		velocity = Vector2.ZERO
 	else:
+		#faz o inimigo ir ate o player
 		velocity = distance * 0.2
 
 	move_and_slide()
-
-func _on_move_timer_timeout():
-	#randomiza a direcao que o inimigo tem que ir
-	direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
 
 func _on_shoot_timer_timeout():
 	#atira a bala
