4ac6eaed355c9ff612cc574744df606117d88b2b
Arthur Mazzardo Naue <arthur_naue@Hotmail.com>
2024-04-16 21:37:56 -0300

    adicionei a cena de morte

--- scripts/demoEndScreen/demoEndScreen.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-class_name demoEndScreen
-
-func _on_replay_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/game/root/game.tscn")
--- scripts/game/entities/components/healthComponent.gd
+++ scripts/game/entities/components/healthComponent.gd
@@ -42,14 +42,18 @@ func Damage(attack: Attack):
 	if health <= 0:
 		#spawna a particula de morte
 		spawnDeathParticle(global_position)
-		#destroi o objeto
-		get_parent().queue_free()
 		#verifica se e um inimigo que morreu
 		if get_parent().is_in_group("enemy"):
 			#spawna uma moeda na posicao do inimigo
 			game.spawnCoin(global_position, enemyType)
 			#retira 1 da quantidade de entidades vivas
 			game.entitiesAlive -= 1
+			#destroi o objeto do inimigo
+			get_parent().queue_free()
+		#verifica se o player morreu
+		elif get_parent().is_in_group("player"):
+			#muda a cena para a de morte
+			get_tree().change_scene_to_file("res://scenes/deathScreen/deathScreen.tscn")
 
 #funcao de spawnar a particula de morte
 func spawnDeathParticle(location: Vector2):
